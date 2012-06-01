394de41beca637fe1d5b6277a55c5678e26d065c
Scott Ritchie <sritchie73@gmail.com>
2012-06-01 11:19:30 +1000

    Forgot to uncomment a line

--- src/gridservice/master/scheduler.py
+++ src/gridservice/master/scheduler.py
@@ -88,7 +88,7 @@ class Scheduler(object):
 				free_nodes = True
 				
 				# Kill any work_units which have no chance of finishing before the deadline.
-				# for unit in self.grid.get_queued():
+				for unit in self.grid.get_queued():
 				 	if (int(time.time()) + unit.job.wall_seconds) > unit.job.deadline:
 				 		unit.kill_msg = "Killed by scheduler: Unable to complete work_unit by deadline."
 				 		unit.kill()
